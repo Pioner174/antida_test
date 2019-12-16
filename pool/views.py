93c7b64bfc58531e5e93fc660a76c5b4694aa8b0
omfg <podsh74@gamil.com>
2019-12-16 16:48:21 +0500

    Добавил стили (Надо подключить bootstrap)

--- pool/views.py
+++ pool/views.py
@@ -1,25 +1,42 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 import pysnooper
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'pool/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'pool/index.html', context)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk = question_id)
-    return render(request, 'pool/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'pool/detail.html'
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'pool/results.html', {'question': question})
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'pool/results.html'
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'pool/index.html', context)
+#
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk = question_id)
+#     return render(request, 'pool/detail.html', {'question': question})
+#
+#
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'pool/results.html', {'question': question})
 
 
 def vote(request, question_id):
